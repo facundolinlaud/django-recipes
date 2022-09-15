c7dc10f618f1292b28e341edaf2d0b7d2ddd86aa
Facundo Linlaud <facundolinlaud@gmail.com>
2022-09-15 11:46:49 +0200

    pr comments: removed BigInt as recipe id

--- app/recipes/models.py
+++ app/recipes/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Recipe(models.Model):
-    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
 
